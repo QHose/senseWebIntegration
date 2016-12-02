73140db132909e79ab5597e0ca44dc1fd194afe3
QHose <mbj@qlik.com>
2016-12-02 11:19:20 +0100

    all works with current selection
    issue when clearing selections, austria selected

--- client/ui/app/DIV integration/table.js
+++ client/ui/app/DIV integration/table.js
@@ -35,6 +35,7 @@ Template.tableUsingWidgets.events({
         //https://help.qlik.com/en-US/sense-developer/3.0/Subsystems/APIs/Content/MashupAPI/Methods/select-method.htm
         require(["js/qlik"], function(qlik) {
             var app = qlik.openApp(Meteor.settings.public.multipleDivAppGuid, qConfig);
+            app.field('Country').clear();
             app.field('Country').select(array, false, true); //works fine, turn toggle to false for multi select
 
         })
@@ -43,9 +44,14 @@ Template.tableUsingWidgets.events({
 })
 
 Template.tableUsingWidgets.onRendered(function() {
+    getCurrentSelections();
+    getCountryList();
+
     this.$('.ui.dropdown')
         .dropdown();
+})
 
+function getCountryList() {
     require.config({
         baseUrl: "http://" + qConfig.host + (qConfig.port ? ":" + qConfig.port : "") + qConfig.prefix + "resources"
     });
@@ -73,14 +79,62 @@ Template.tableUsingWidgets.onRendered(function() {
             this.countryList = new ReactiveVar(countries);
             var str = "";
             $.each(reply.qListObject.qDataPages[0].qMatrix, function(key, value) {
-                str += '<div class="item" data-value="'+value[0].qElemNumber+ '" >' + value[0].qText + '</div>';
+                str += '<div class="item" data-value="' + value[0].qElemNumber + '" >' + value[0].qText + '</div>';
             });
             this.$('#list').html(str);
         });
         //https://help.qlik.com/en-US/sense-developer/3.0/Subsystems/APIs/Content/MashupAPI/Methods/createList-method.htm
     })
+}
 
-})
+function getCurrentSelections() { //source: http://snipplr.com/view/100939/custom-current-selections/
+    require.config({
+        baseUrl: "http://" + qConfig.host + (qConfig.port ? ":" + qConfig.port : "") + qConfig.prefix + "resources"
+    });
+
+    require(["js/qlik"], function(qlik) {
+        var app = qlik.openApp(Meteor.settings.public.multipleDivAppGuid, qConfig);
+        console.log('App layout object: ', app);
+        app.getList("CurrentSelections", function(reply) {
+
+            selectionslist = "";
+            var mySelectedFields = reply.qSelectionObject.qSelections;
+            console.log(mySelectedFields);
+
+            //loop through selected fields
+            var mySelectedFields_arrayLength = mySelectedFields.length;
+            for (var i = 0; i < mySelectedFields_arrayLength; i++) {
+                var text = mySelectedFields[i].qField + " ";
+                console.log(mySelectedFields[i].qField);
+
+                if (mySelectedFields[i].qLocked == true) {
+                    var labeltype = 'label label-warning'
+                } else {
+                    var labeltype = 'label-success';
+                }
+
+                console.log(mySelectedFields[i].qLocked);
+
+                //loop through selected field values
+                var currentFieldValues = mySelectedFields[i].qSelectedFieldSelectionInfo
+                var currentFieldValues_arrayLength = currentFieldValues.length;
+
+                for (var y = 0; y < currentFieldValues_arrayLength; y++) {
+                    text += "<span class='label " + labeltype + "'>" + currentFieldValues[y].qName + "</span> ";
+                }
+
+                if (i == 0) {
+                    selectionslist = text;
+                } else {
+                    selectionslist += text;
+                }
+            }
+
+            console.log(selectionslist);
+            $("#myselections").html(selectionslist);
+        });
+    })
+}
 
 Template.tableUsingWidgets.onCreated(function() {
     this.table = new ReactiveVar('test');
