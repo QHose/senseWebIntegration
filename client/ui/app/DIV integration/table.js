3020f824405b3f502b7bada4d43028e1d018aa9f
QHose <mbj@qlik.com>
2016-12-02 10:50:02 +0100

    javascript explanation added

--- client/ui/app/DIV integration/table.js
+++ client/ui/app/DIV integration/table.js
@@ -4,7 +4,7 @@ Template.tableUsingWidgets.helpers({
     countries() {
         if (Template.instance().countryList) {
             var countries = Template.instance().countryList.get();
-            // console.log('helper: app.createList returned these countries', countries);
+            console.log('helper: app.createList returned these countries', countries);
             countries.forEach
             return countries;
         }
@@ -12,14 +12,14 @@ Template.tableUsingWidgets.helpers({
     headers() {
         if (Template.instance().table) {
             var headers = Template.instance().table.get().headers;
-            // console.log('helper: table has these headers', headers);
+            console.log('helper: table has these headers', headers);
             return Template.instance().table.get().headers;
         }
     },
     rows() {
         if (Template.instance().table) {
             var rows = Template.instance().table.get().rows;
-            // console.log('helper: table has these rows', rows);
+            console.log('helper: table has these rows', rows);
             return rows;
         }
     }
@@ -27,15 +27,15 @@ Template.tableUsingWidgets.helpers({
 
 Template.tableUsingWidgets.events({
     'change .country.dropdown': function(event, template) {
-        var selected = template.$('.countryList').val(); //returns ["france", "united kingdom"]
-        var array = selected.split(','); //make an array of selections
+        var selected = template.$('.countryList').val(); //returns [1, 5]
+        var array = selected.split(',').map(Number); //make an array of selections
         console.log(array);
 
         //make the selection in Qlik Sense
         //https://help.qlik.com/en-US/sense-developer/3.0/Subsystems/APIs/Content/MashupAPI/Methods/select-method.htm
         require(["js/qlik"], function(qlik) {
             var app = qlik.openApp(Meteor.settings.public.multipleDivAppGuid, qConfig);
-            app.field('Country').selectMatch(array[0], true, true); //works fine
+            app.field('Country').select(array, false, true); //works fine, turn toggle to false for multi select
 
         })
 
@@ -73,7 +73,7 @@ Template.tableUsingWidgets.onRendered(function() {
             this.countryList = new ReactiveVar(countries);
             var str = "";
             $.each(reply.qListObject.qDataPages[0].qMatrix, function(key, value) {
-                str += '<div class="item" >' + value[0].qText + '</div>';
+                str += '<div class="item" data-value="'+value[0].qElemNumber+ '" >' + value[0].qText + '</div>';
             });
             this.$('#list').html(str);
         });
@@ -95,13 +95,11 @@ Template.tableUsingWidgets.onCreated(function() {
         var app = qlik.openApp(Meteor.settings.public.multipleDivAppGuid, qConfig);
 
         // Returns a table object of type QTable, which is initially empty but that eventually will contain data. The table object will be updated when selection state changes.
-        // var table = app.createTable(["Country", "City"], ["Sum(SalesAmount)"], { rows: 5 });
-        table = app.createTable(["Country", "City"], ["Sum(SalesAmount)"], { rows: 30 });
+        table = app.createTable(["Country", "City"], ["Sum(SalesAmount)"], { rows: 3000 });
 
         var listener = function() {
             console.log('table is: ', table);
             reactiveTable.set(table);
-            // table.OnData.unbind(listener); //unregister the listener when no longer notification is needed.
         };
         table.OnData.bind(listener); //bind the listener
     })
