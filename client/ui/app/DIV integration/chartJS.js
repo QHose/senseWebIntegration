0c2f3a541af509e7d5a774cc960ce2e75e40e1bb
QHose <mbj@qlik.com>
2016-12-16 13:41:54 +0100

    chart.js chart added

---
+++ client/ui/app/DIV integration/chartJS.js
@@ -0,0 +1,74 @@
+//http://www.chartjs.org/docs/#getting-started-creating-a-chart
+Template.chartJS.onRendered(function() {
+	getTable();
+})
+
+function getTable() {
+    require.config({
+        baseUrl: "http://" + qConfig.host + (qConfig.port ? ":" + qConfig.port : "") + qConfig.prefix + "resources"
+    });
+
+    require(["js/qlik"], function(qlik) {
+        var app = qlik.openApp(Meteor.settings.public.multipleDivAppGuid, qConfig);
+
+        // Returns a table object of type QTable, which is initially empty but that eventually will contain data. 
+        // The table object will be updated when selection state changes.
+        table = app.createTable(["Country"], ["Sum(SalesAmount)"], { rows: 3000 });
+
+        var listener = function() {
+            console.log('table is: ', table);
+            drawChartJs(table);
+        };
+        table.OnData.bind(listener); //bind the listener
+    })
+}
+
+function drawChartJs(table) {
+    var ctx = $("#myChart");
+    var rows = table.rows;
+    var labels = _.map(rows, function(row){
+    	return row.cells["0"].qText
+    })
+    var data = _.map(rows, function(row){
+    	return 	row.measures["0"].qNum;
+    })
+
+    console.log('labels array is ', labels);
+    console.log('data array', data);
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels, //["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: 'Sales Amount',
+                data: data, //[12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            }
+        }
+    });
+}
--- client/ui/app/DIV integration/googleChart.js
+++ client/ui/app/DIV integration/googleChart.js
@@ -15,8 +15,8 @@ function getTable(template) {
         var app = qlik.openApp(Meteor.settings.public.multipleDivAppGuid, qConfig);
 
         // Returns a table object of type QTable, which is initially empty but that eventually will contain data. 
-        // The table object will be updated when selection state changes.
-        table = app.createTable(["Country"], ["Sum(SalesAmount)"], { rows: 1000 });
+        // The table object will be updated when selection state changes. https://help.qlik.com/en-US/sense-developer/3.1/Subsystems/APIs/Content/MashupAPI/Methods/createTable-method.htm
+        table = app.createTable(["Country", "Category"], ["Sum(SalesAmount)"], { rows: 1000 });
 
         var listener = function() {
             console.log('We received the populated table from Qlik Sense: ', table);
@@ -24,27 +24,27 @@ function getTable(template) {
                 //.qHyperCube.qDataPages["0"].qMatrix["0"]["0"].qText
                 //.qHyperCube.qDataPages["0"].qMatrix["0"][1].qNum
                 var itemArray = [];
+                // console.log('currentItem', currentItem);
                 itemArray.push(currentItem["0"].qText);
                 itemArray.push(currentItem[1].qNum);
                 return itemArray;
             })
-            console.log('googleFormattedData: ', googleFormattedData);
-            createGoogleChart(googleFormattedData);
+            createGoogleMapChart(googleFormattedData);
         };
         table.OnData.bind(listener); //bind the listener
     })
 }
 
-function createGoogleChart(data) {
+function createGoogleMapChart(data) {
     //https://developers.google.com/chart/interactive/docs/gallery/geochart
     google.charts.load('upcoming', { 'packages': ['geochart'] });
     google.charts.setOnLoadCallback(drawRegionsMap)
     var data2 = data;
-   
+
     function drawRegionsMap() {
         var dataWithHeaders = [];
         dataWithHeaders.push(['Country', 'SalesAmount']);
-        _.each(data2, function(item){
+        _.each(data2, function(item) {
             dataWithHeaders.push(item);
         })
         console.log('dataWithHeaders', dataWithHeaders);
@@ -53,7 +53,20 @@ function createGoogleChart(data) {
         var options = {};
 
         var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+        // The select handler. Call the chart's getSelection() method
+        function selectHandler() {
+            var selectedItem = chart.getSelection()[0];
+            if (selectedItem) {
+                var value = data.getValue(selectedItem.row, selectedItem.column);
+                alert('The user selected ' + value);
+            }
+        }
+
+        // Listen for the 'select' event, and call my function selectHandler() when
+        // the user selects something on the chart.
+        google.visualization.events.addListener(chart, 'select', selectHandler);
 
         chart.draw(data, options);
     }
 }
+
