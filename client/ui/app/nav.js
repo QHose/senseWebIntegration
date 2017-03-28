b5d49a652d0856a23645756b89d2f378eb1999b5
QHose <mbj@qlik.com>
2017-03-28 19:35:33 +0200

    new QAP selector

--- client/ui/app/nav.js
+++ client/ui/app/nav.js
@@ -4,6 +4,23 @@ Template.webIntegrationMessage.onRendered(function() {
 })
 
 Template.sideMenu.onRendered(function() {
+	Session.set('QAPOnly', false);
     this.$('.ui.dropdown')
         .dropdown();
+})
+
+Template.sideMenu.events({
+    'change .checkbox.template' (event, template) {
+        if (event.target.checked) {
+        	Session.set('QAPOnly', true);
+        } else {
+        	Session.set('QAPOnly', false);
+        }
+    }
+})
+
+Template.sideMenu.helpers({
+    checked() {
+        return Session.get('QAPOnly')? 'checked':'';
+    }
 })
--- client/ui/uiHelpers.js
+++ client/ui/uiHelpers.js
@@ -24,13 +24,19 @@ Template.registerHelper('appURL', function() {
         appGUID = Meteor.settings.public.multipleDivAppGuid
     }
 
+    if (Session.get('QAPOnly')) //show the link without menu
+    //http://presales1:81/single/?appid=78f7312a-85c1-4428-b93e-0f2c1ea04b64&sheet=PumJhh&opt=currsel
+url = (qConfig.isSecure ? "https://" : "http://") + qConfig.host + (qConfig.port ? ":" + qConfig.port : "") + qConfig.prefix + "single/?appid=" + appGUID + "&sheet=VUkssjz&opt=currsel";
+
+else {
     url = (qConfig.isSecure ? "https://" : "http://") + qConfig.host + (qConfig.port ? ":" + qConfig.port : "") + qConfig.prefix + "sense/app/" + appGUID + '/sheet/VUkssjz/state/analysis';
+}
 
     // console.log('the generated IFrame URL is: ' + url);
     return url;
 });
 
-Template.registerHelper('appGUID', function(){
+Template.registerHelper('appGUID', function() {
     return Meteor.settings.public.multipleDivAppGuid;
 })
 
@@ -60,12 +66,16 @@ Template.registerHelper('QlikSenseDemoURL', function() {
     return QlikSenseDemoURL();
 });
 
-QlikSenseDemoURL = function QlikSenseDemoURL(){
-    return "http://" + Meteor.settings.public.host + ":" + Meteor.settings.public.port+"/"+Meteor.settings.public.virtualProxyClientUsage;
+//only show stuff if it is part of qlik sense or QAP
+Template.registerHelper('QSE', function() {
+    return Session.get('QAPOnly');
+});
+
+QlikSenseDemoURL = function QlikSenseDemoURL() {
+    return "http://" + Meteor.settings.public.host + ":" + Meteor.settings.public.port + "/" + Meteor.settings.public.virtualProxyClientUsage;
 }
 
 //url of the Qlik sense server
 Template.registerHelper('cityBikeURL', function() {
-    return "http://" + Meteor.settings.public.host + ":" + Meteor.settings.public.port+"/"+Meteor.settings.public.virtualProxyClientUsage+"/extensions/DemoMashup/home.html";
+    return "http://" + Meteor.settings.public.host + ":" + Meteor.settings.public.port + "/" + Meteor.settings.public.virtualProxyClientUsage + "/extensions/DemoMashup/home.html";
 });
-
