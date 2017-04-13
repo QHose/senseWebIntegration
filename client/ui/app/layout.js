0b962d66545ca34c02ab41be1e1755923a24c0fe
QHose <mbj@qlik.com>
2017-04-13 09:07:16 +0200

    new QAP menus

--- client/ui/app/layout.js
+++ client/ui/app/layout.js
@@ -1,3 +1,6 @@
 Template.layout.onCreated(function() {
     checkAuthenticatedInQlik(encodeURIComponent(window.location.href));
 })
+
+process.env.ROOT_URL = 'http://' + Meteor.settings.public.host;
+console.log('********* Meteor runs on host ROOT_URL: ', process.env.ROOT_URL);
