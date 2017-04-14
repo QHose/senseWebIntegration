cabc2d9fd4e42acc279e2b49152e49b80fc7b9b0
QHose <mbj@qlik.com>
2017-04-14 08:25:19 +0200

    hide technical information in dropdown

--- client/ui/app/layout.js
+++ client/ui/app/layout.js
@@ -2,5 +2,13 @@ Template.layout.onCreated(function() {
     checkAuthenticatedInQlik(encodeURIComponent(window.location.href));
 })
 
+
+Template.regionLayout.onRendered(function() {
+    this.$('.ui.accordion')
+        .accordion();
+})
+
+
+
 process.env.ROOT_URL = 'http://' + Meteor.settings.public.host;
 console.log('********* Meteor runs on host ROOT_URL: ', process.env.ROOT_URL);
