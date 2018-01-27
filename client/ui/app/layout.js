76e8b804c19e5a4144ef092f16bb14e4769cff00
Martijn Biesbroek <mbj@qlik.com>
2018-01-27 11:19:32 +0100

    test

--- client/ui/app/layout.js
+++ client/ui/app/layout.js
@@ -1,7 +1,7 @@
 Template.regionLayout.onCreated(function() {
-  //   checkAuthenticatedInQlik(encodeURIComponent(window.location.href));
+    checkAuthenticatedInQlik(encodeURIComponent(window.location.href));
 });
 
 Template.regionLayout.onRendered(function() {
-  this.$(".ui.accordion").accordion();
+    this.$(".ui.accordion").accordion();
 });
